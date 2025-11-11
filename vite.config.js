77e317047d07b28a263307eaa4c583786d75a211
99-09 <3081628907@qq.com>
2025-11-11 16:44:34 +0800

    修正脚本路径，移除.git后缀

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/99-09/myproject.git/' });
+  base: '/https://github.com/99-09/myproject/' });
